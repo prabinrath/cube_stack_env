0cbf83a2e57937d4d7fc5ddb2b7f517892b0ddb7
Prabin Rath <prabinrath123@gmail.com>
2023-05-06 02:02:55 -0700

    pick n place moveit working

--- cube_stack_env/scripts/moveit_handler_hardware.py
+++ cube_stack_env/scripts/moveit_handler_hardware.py
@@ -6,8 +6,6 @@ import numpy as np
 from scipy.interpolate import CubicHermiteSpline
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
-from threading import Lock
-import time
 
 class MoveItHandler():
     def __init__(self, robot_description, z_offset, scaling=1.0):
@@ -22,8 +20,9 @@ class MoveItHandler():
         print('Arm Reference: ', self.move_arm.get_pose_reference_frame(), '--->', self.move_arm.get_end_effector_link())
 
         self.n_pillars = 10
-        self.joint_state_lock = Lock()
         self.joint_states = np.zeros((5,), dtype=np.float64)
+        # helps in preventing the jerk while switching moveit group
+        self.arm_command_bkp = np.zeros((5,), dtype=np.float64)
         # ground plane
         box_pose = geometry_msgs.msg.PoseStamped()
         box_pose.header.frame_id = self.robot.get_planning_frame()
@@ -41,7 +40,44 @@ class MoveItHandler():
         box_pose.pose.position.y = 0.0
         box_pose.pose.position.z = - self.z_offset + 0.2
         box_name = "back"
-        self.scene.add_box(box_name, box_pose, size=(0.01,0.4,0.4))
+        self.scene.add_box(box_name, box_pose, size=(0.01,0.8,0.8))
+        # top plane
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.frame_id = self.robot.get_planning_frame()
+        box_pose.pose.orientation.w = 1.0
+        box_pose.pose.position.x = 0.0
+        box_pose.pose.position.y = 0.0
+        box_pose.pose.position.z = - self.z_offset + 0.3
+        box_name = "top"
+        self.scene.add_box(box_name, box_pose, size=(0.8,0.8,0.01))
+        # left plane
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.frame_id = self.robot.get_planning_frame()
+        box_pose.pose.orientation.w = 1.0
+        box_pose.pose.position.x = 0.0
+        box_pose.pose.position.y = -0.2
+        box_pose.pose.position.z = - self.z_offset + 0.2
+        box_name = "left"
+        self.scene.add_box(box_name, box_pose, size=(0.8,0.01,0.8))
+        # right plane
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.frame_id = self.robot.get_planning_frame()
+        box_pose.pose.orientation.w = 1.0
+        box_pose.pose.position.x = 0.0
+        box_pose.pose.position.y = 0.2
+        box_pose.pose.position.z = - self.z_offset + 0.2
+        box_name = "right"
+        self.scene.add_box(box_name, box_pose, size=(0.8,0.01,0.8))
+        # shoulder safety pillars
+        for i in range(self.n_pillars):
+            box_pose = geometry_msgs.msg.PoseStamped()
+            box_pose.header.frame_id = self.robot.get_planning_frame()
+            box_pose.pose.orientation.w = 1.0
+            box_pose.pose.position.x = 0.06*np.sin(i*2*np.pi/self.n_pillars)
+            box_pose.pose.position.y = 0.06*np.cos(i*2*np.pi/self.n_pillars)
+            box_pose.pose.position.z = 0.03
+            box_name = "pillar_shoulder_safety_"+str(i)
+            self.scene.add_box(box_name, box_pose, size=(0.01,0.01,0.06))
     
     def __del__(self):
         rospy.loginfo('Shutting Down MoveIt Commander')
@@ -54,16 +90,17 @@ class MoveItHandler():
             box_pose.pose.orientation.w = 1.0
             box_pose.pose.position.x = goal_position[0] + 0.06*np.sin(i*2*np.pi/self.n_pillars)
             box_pose.pose.position.y = goal_position[1] + 0.06*np.cos(i*2*np.pi/self.n_pillars)
-            box_pose.pose.position.z = - self.z_offset + 0.025
+            box_pose.pose.position.z = - self.z_offset + 0.03
             box_name = "pillar_obs_"+str(i)
-            self.scene.add_box(box_name, box_pose, size=(0.01,0.01,0.05))
+            self.scene.add_box(box_name, box_pose, size=(0.01,0.01,0.06))
 
         self.move_arm.set_position_target(goal_position)
         self.move_arm.set_goal_tolerance(0.001)
         success, traj, _, _ = self.move_arm.plan()
         if not success:
             rospy.logwarn('Plan Failed')
-        success = self.execute_trajectory(traj, "arm") and success
+        else:
+            success = self.execute_trajectory(traj, "arm") and success
         self.move_arm.clear_pose_targets()
 
         for i in range(self.n_pillars):
@@ -84,14 +121,13 @@ class MoveItHandler():
         success, traj, _, _ = self.move_arm.plan()
         if not success:
             rospy.logwarn('Plan Failed')
-        success = self.execute_trajectory(traj, "arm") and success
+        else:
+            success = self.execute_trajectory(traj, "arm") and success
         self.move_arm.clear_pose_targets()
         return success, traj
     
     def joint_state_callback(self, msg):
-        self.joint_state_lock.acquire()
         self.joint_states = np.array(msg.position, dtype=np.float64)
-        self.joint_state_lock.release()
 
     def execute_trajectory(self, traj, group):
         if group == "arm":
@@ -107,24 +143,25 @@ class MoveItHandler():
             dy = data[:,1,:]
             interp = CubicHermiteSpline(tx, y, dy)
             yq = interp(np.linspace(0, 1, 100))
-            self.joint_state_lock.acquire()
             grab_pos = self.joint_states[-1]
-            self.joint_state_lock.release()
             yq[:,4] = grab_pos
             for yi in yq:
                 msg = Float64MultiArray()
                 msg.data = yi.tolist()
-                self.command_pub.publish(msg)
-                time.sleep(self.scaling/100)
+                error = np.linalg.norm(yi-self.joint_states)
+                while error > 0.2:
+                    error = np.linalg.norm(yi-self.joint_states)
+                    self.command_pub.publish(msg)               
+                    # rospy.sleep(self.scaling/1000) 
+                self.arm_command_bkp = yi
+            rospy.sleep(2)
             return True
         elif group == "gripper":
             msg = Float64MultiArray()
-            self.joint_state_lock.acquire()
-            msg.data = self.joint_states.tolist()
-            self.joint_state_lock.release()
+            msg.data = self.arm_command_bkp
             msg.data[-1] = traj
             self.command_pub.publish(msg)
-            time.sleep(2)  
+            rospy.sleep(2)
             return True
         rospy.logwarn('Execution Failed')
         return False       
--- cube_stack_env/scripts/pick_n_place.py
+++ cube_stack_env/scripts/pick_n_place.py
@@ -6,15 +6,18 @@ DUMMY_AXIS_OFFSET = 0.01
 
 def main():
     rospy.init_node("pick_n_place")
-    robot_mp = MoveItHandler(robot_description='/cube_stack_arm/robot_description', z_offset=BASE_Z_OFFSET, scaling=2.0)
+    robot_mp = MoveItHandler(robot_description='/cube_stack_arm/robot_description', z_offset=BASE_Z_OFFSET)
 
-    for i in range(2):
-        robot_mp.home()    
-        goal_pos = [0.2, 0.1, 0.0+DUMMY_AXIS_OFFSET]
-        robot_mp.gripper_open()
-        robot_mp.move_to([goal_pos[0], goal_pos[1], goal_pos[2]])
-        robot_mp.gripper_close()
-        # robot_mp.home()
-        rospy.loginfo(f"Task {i+1} completed")
+    # for i in range(5):
+    robot_mp.home()
+    robot_mp.gripper_open()
+    goal_pos = [0.15, -0.1, -0.04+DUMMY_AXIS_OFFSET]
+    robot_mp.move_to([goal_pos[0], goal_pos[1], goal_pos[2]])
+    robot_mp.gripper_close()
+    goal_pos = [0.15, 0.1, 0.025+DUMMY_AXIS_OFFSET]
+    robot_mp.move_to([goal_pos[0], goal_pos[1], goal_pos[2]])
+    robot_mp.gripper_open()
+    robot_mp.home()
+    # rospy.loginfo(f"Task {i+1} completed")
     
 main()
